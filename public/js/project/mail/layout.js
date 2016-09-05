7a4cbba9455096be646955485763d7ca51f66cce
Panagiotis Giagkoulas <p.giagkoulas@safemarket.gr>
2016-09-05 13:40:18 +0300

    Dynamic top menu for mail

--- public/js/project/mail/layout.js
+++ public/js/project/mail/layout.js
@@ -5,8 +5,27 @@ $(document).ready(function () {
     $("#LeftMenu").jqxMenu({width: 'auto', height:'99%', mode: 'vertical', theme: theme});
     $("#LeftMenu").css('visibility', 'visible');
 
+
+    /*
+    Dynamically add elements to TopMenu
+     */
+    var $numOfButtons = 3;
+    var $buttons = [
+        {name:'Create', id:'crt'},
+        {name:'Delete', id:'dlt'},
+        {name:'Hide', id:'hd'}
+    ];
+
+    for($i=0; $i<$numOfButtons; $i++) {
+        var $myDiv = ('<li id="'+($buttons[$i]).id+'">'+($buttons[$i]).name+'<li>');
+        $('[name="myList"]').append($myDiv);
+    }
+
+
+
     $("#TopMenu").jqxMenu({width: 'auto', mode: 'horizontal', theme: theme});
     $("#TopMenu").css('visibility', 'visible');
+
 
     $("#MailGrid").jqxGrid(
         {
